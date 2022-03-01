2510136a607f14d6d87113a2644c3813a58bc503
ji <jiyongprk@gmail.com>
2022-03-01 15:51:22 -0500

    start something on js

---
+++ main.js
@@ -0,0 +1,8 @@
+const title = document.querySelector('.title');
+const info = document.querySelector('.info');
+const text = document.querySelector('.text');
+const submitBtn = document.querySelector('.submit-btn');
+const list = document.querySelector('.list');
+
+
+
