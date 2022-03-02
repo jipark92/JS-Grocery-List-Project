13dab2aafb6bdf1f8cb42bc65eeecbd41d171684
ji <jiyongprk@gmail.com>
2022-03-02 10:42:25 -0500

    work on more of evreything

--- main.js
+++ main.js
@@ -6,11 +6,11 @@ const list = document.querySelector('.list');
 const groceryList = document.querySelector('.grocery-container');
 const editBtn = document.querySelector('.edit-btn');
 const deleteBtn = document.querySelector('.delete-btn');
+const itemsCont = document.querySelector('.items');
 
 
-let editElement;
-let editFlag = false;
-let editID = "";
+
+let num = 0;
 
 
 
@@ -20,9 +20,27 @@ function submitButton(){
         console.log(textInput.value);
         itemTitle.textContent = textInput.value;
         groceryList.style.visibility = "visible";
+        
+        if (textInput.value === "") {
+            alert('Error: empty text');
+            num = -1;
+            groceryList.style.visibility = "hidden";
+            // deleteBtn.style.visibility = "hidden";
+            // editBtn.style.visibility = "hidden";
+        }
+        num++;
+        console.log(num);
+        // if (num === 1){
+        //     const paragraph = document.createElement('p');  
+        //     paragraph.setAttribute('class', 'item-title1'); 
+        //     itemsCont.appendChild(paragraph);
+        //     itemTitle.textContent = textInput.value;
+        // } 
     });
 }
 submitButton();
+
+
 
 //delete button
 function deleteButton() {
