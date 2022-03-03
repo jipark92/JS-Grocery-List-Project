b1b8ef548c480a38ca738f86f40ed4fc81cc553a
ji <jiyongprk@gmail.com>
2022-03-03 01:53:02 -0500

    finish error function

--- main.js
+++ main.js
@@ -7,21 +7,37 @@ const inputBox = document.querySelector('.input-box');
 const submitBtns = document.querySelector('.submit-btn');
 const list = document.querySelector('.list');
 const groceryList = document.querySelector('.grocery-container');
-
+const clearBtn = document.querySelector('.clear-btn');
 const deleteBtn = document.querySelector('.delete-btn');
 const itemText = document.querySelector('.text');
 const itemBtn = document.querySelector('.btn');
-
-
-
+const errorText = document.querySelector('.error');
+const container = document.querySelector('.container');
+const buttons = document.querySelector('button');
 let num = 0;
+
+
 
 // submit text and list it
 submitBtns.addEventListener('click', ()=>{
 
     
     if (inputBox.value === ''){
-        alert('error');
+        function alert (){
+            container.style.boxShadow= "5px 5px 25px red";
+            buttons.style.backgroundColor = "red";
+            clearBtn.style.backgroundColor = "red";
+            errorText.textContent = "Error: No Text";
+            errorText.style.color = "red";
+            //remove red color style
+            setTimeout(()=>{
+                container.style.boxShadow= "5px 5px 25px #10B981";
+                buttons.style.backgroundColor = "#10B981";
+                clearBtn.style.backgroundColor = "#10B981";
+                errorText.textContent = "";
+            },500)
+        }
+        alert();
 
     }else if (num === 0) {
         //  create item-title
