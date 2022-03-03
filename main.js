462d702aeff4e84b22d9360a5126fa70cf97ae59
ji <jiyongprk@gmail.com>
2022-03-03 02:10:14 -0500

    delete,clear,submit all working!

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
-// let itemTitle = document.querySelector('.item-title');
-
 const info = document.querySelector('.info');
-
 const inputBox = document.querySelector('.input-box');
-
 const submitBtns = document.querySelector('.submit-btn');
 const list = document.querySelector('.list');
 const groceryList = document.querySelector('.grocery-container');
@@ -16,12 +12,8 @@ const container = document.querySelector('.container');
 const buttons = document.querySelector('button');
 let num = 0;
 
-
-
 // submit text and list it
 submitBtns.addEventListener('click', ()=>{
-
-    
     if (inputBox.value === ''){
         function alert (){
             container.style.boxShadow= "5px 5px 25px red";
@@ -38,7 +30,6 @@ submitBtns.addEventListener('click', ()=>{
             },500)
         }
         alert();
-
     }else if (num === 0) {
         //  create item-title
         const elementZero = document.createElement('div');
@@ -57,14 +48,11 @@ submitBtns.addEventListener('click', ()=>{
         btns.setAttribute('class', 'delete-btn');
         btns.textContent = 'delete';
         
-
         //append 
         itemBtn.appendChild(btns);
        
-
         //add to num
         num++;
-        console.log(num)
 
         //delete
         btns.addEventListener('click',()=>{
@@ -72,98 +60,22 @@ submitBtns.addEventListener('click', ()=>{
             itemText.removeChild(elementZero);
             console.log('clicked');
         })
-        
-        
+
+        //clear button
+        function clearAll () {
+            clearBtn.addEventListener('click', ()=>{
+                itemBtn.removeChild(btns);
+                itemText.removeChild(elementZero);
+                inputBox.textContent = "";
+             })
+        }
+        clearAll();
     } else {
         num = 0;
     }
 });
 
-
-
-
-
-
-
-
 //empty string input on website load
 window.addEventListener('DOMContentLoaded', ()=>{
     inputBox.value = "";
 })
-
-
-
-
-
-
-
-
-
-
-// let num = 0;
-
-
-
-// // // submit input text.
-// function submitButton(){
-//     submitBtns.addEventListener('click', () => {
-//         console.log(textInput.value);
-//         itemTitle.textContent = textInput.value;
-//         groceryList.style.visibility = "visible";
-        
-//         if (textInput.value === "") {
-//             alert('Error: empty text');
-//             num = -1;
-//             groceryList.style.visibility = "hidden";
-//             // deleteBtn.style.visibility = "hidden";
-//             // editBtn.style.visibility = "hidden";
-//         }
-//         num++;
-//         console.log(num);
-//         if (num === 1){
-//             const element = document.createElement('div');  
-//             element.setAttribute('class', 'item-title1'); 
-//             itemsCont.appendChild(element);
-//             itemTitle.textContent = textInput.value;
-//         } 
-//         // else if (num === 2) {
-//         //     const paragraph = document.createElement('div');  
-//         //     paragraph.setAttribute('class', 'item-title2'); 
-//         //     itemsCont.appendChild(paragraph);
-//         //     itemTitle.textContent = textInput.value;
-//         // } else {
-            
-//         // }
-//     });
-// }
-// submitButton();
-
-
-
-// //delete button
-// function deleteButton() {
-//     deleteBtn.addEventListener('click', () => {
-//        itemTitle.textContent = "";
-//        deleteBtn.remove();
-//        editBtn.remove();
-//     })
-// }
-// deleteButton();
-
-
-// //edit button
-// // function editButton() {
-// //     editBtn.addEventListener('click', () => {
-// //        itemTitle.textContent = "";
-// //        deleteBtn.remove();
-// //        editBtn.remove();
-// //     })
-// // }
-// // editButton();
-
-
-// //empty string input on website load
-// window.addEventListener('DOMContentLoaded', ()=>{
-//     textInput.value = "";
-// } )
-
