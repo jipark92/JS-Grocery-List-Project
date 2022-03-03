dfc51cebed2a06c2626db4f06d97e5b76866479e
ji <jiyongprk@gmail.com>
2022-03-03 10:10:40 -0500

    more tweaks

--- main.js
+++ main.js
@@ -1,88 +1,86 @@
-const info = document.querySelector('.info');
-const inputBox = document.querySelector('.input-box');
-const submitBtns = document.querySelector('.submit-btn');
-const list = document.querySelector('.list');
-const groceryList = document.querySelector('.grocery-container');
-const clearBtn = document.querySelector('.clear-btn');
-const deleteBtn = document.querySelector('.delete-btn');
-const itemText = document.querySelector('.text');
-const itemBtn = document.querySelector('.btn');
-const errorText = document.querySelector('.error');
-const container = document.querySelector('.container');
-const buttons = document.querySelector('button');
-let num = 0;
+function groceryList() {
+    const inputBox = document.querySelector('.input-box');
+    const submitBtns = document.querySelector('.submit-btn');
+    const clearBtn = document.querySelector('.clear-btn');
+    const itemText = document.querySelector('.text');
+    const itemBtn = document.querySelector('.btn');
+    const errorText = document.querySelector('.error');
+    const container = document.querySelector('.container');
+    const buttons = document.querySelector('button');
+    let num = 0;
 
-// submit text and list it
-submitBtns.addEventListener('click', ()=>{
-    if (!inputBox.value){
-        function alert (){
-            container.style.boxShadow= "5px 5px 25px red";
-            buttons.style.backgroundColor = "red";
-            clearBtn.style.backgroundColor = "red";
-            errorText.textContent = "Error: No Text";
-            errorText.style.color = "red";
-            //remove red color style
-            setTimeout(()=>{
-                container.style.boxShadow= "5px 5px 25px #10B981";
-                buttons.style.backgroundColor = "#10B981";
-                clearBtn.style.backgroundColor = "#10B981";
-                errorText.textContent = "";
-            },500)
-        };
-        alert();
-    }else if (num <= 0 ) {
-        //  create item-title
-        const elementZero = document.createElement('div');
-        elementZero.setAttribute('class', 'item-title');
-     
-        //select item-title
-        let itemZero = document.querySelector('.item-title');
-
-        //append 
-       itemText.appendChild(elementZero);
-
-        //add text
-        elementZero.textContent = inputBox.value;
-
-        //create delete-btn
-        const btns = document.createElement('button');
-        btns.setAttribute('class', 'delete-btn');
-        btns.textContent = 'delete';
+    // submit text and list it
+    submitBtns.addEventListener('click', ()=>{
+        if (!inputBox.value){
+            function alert (){
+                container.style.boxShadow= "5px 5px 25px red";
+                buttons.style.backgroundColor = "red";
+                clearBtn.style.backgroundColor = "red";
+                errorText.textContent = "Error: No Text";
+                errorText.style.color = "red";
+                //remove red color style
+                setTimeout(()=>{
+                    container.style.boxShadow= "5px 5px 25px #10B981";
+                    buttons.style.backgroundColor = "#10B981";
+                    clearBtn.style.backgroundColor = "#10B981";
+                    errorText.textContent = "";
+                },300)
+            };
+            alert();
+        }else if (num === 0 && inputBox.value) {
+            //  create item-title
+            const elementZero = document.createElement('div');
+            elementZero.setAttribute('class', 'item-title');
         
-        //append button
-        itemBtn.appendChild(btns);
-       
-       
-        //add to num
-        num++;
-        console.log(num)
-        //delete
-        btns.addEventListener('click',()=>{
-            itemBtn.removeChild(btns);
-            itemText.removeChild(elementZero);
-            console.log('clicked');
-        });
+            // select item-title
+            let itemZero = document.querySelector('.item-title');
 
-        //clear button
-        function clearAll () {
-            clearBtn.addEventListener('click', ()=>{
-                if (true){
-                    itemBtn.removeChild(btns);
-                } else {
-                    return;
-                }
+            //append 
+            itemText.appendChild(elementZero);
+
+            //add text
+            elementZero.textContent = inputBox.value;
+            inputBox.value = ""
+
+            //create delete-btn
+            const btns = document.createElement('button');
+            btns.setAttribute('class', 'delete-btn');
+            btns.textContent = 'delete';
+            
+            //append button
+            itemBtn.appendChild(btns);
+        
+            //add to num
+            num++;
+            console.log(num)
+            //delete
+            btns.addEventListener('click',()=>{
+                itemBtn.removeChild(btns);
                 itemText.removeChild(elementZero);
-                inputBox.value = "";
-             })
-        };
-        clearAll();
-        inputBox.value = "";
-    } else {
-        num--;
-    }
-});
+                console.log('clicked');
+            });
 
-//empty string input on website load
-window.addEventListener('DOMContentLoaded', ()=>{
-    inputBox.value = "";
-})
+            //clear button
+            function clearAll () {
+                clearBtn.addEventListener('click', ()=>{
+                    if (true){
+                        itemBtn.removeChild(btns);
+                    } else {
+                        return;
+                    }
+                    itemText.removeChild(elementZero);
+                    inputBox.value = "";
+                })
+            };
+            clearAll();
+        } else {
+            num = 0;
+        }
+    });
+
+    //empty string input on website load
+    window.addEventListener('DOMContentLoaded', ()=>{
+        inputBox.value = "";
+    })
+};
+groceryList();
